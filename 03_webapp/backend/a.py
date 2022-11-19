5b7a31b79ef0c6e99cfd61a488e07243d91c13b4
Blackjtsai <blackjtsai@gmail.com>
2022-11-19 13:43:56 +0800

    19

--- 03_webapp/backend/a.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-
-file_path = os.path.join(os.getcwd(), '03_webapp', 'backend', 'data.json')
-with open(file_path) as f:
-    data = json.load(f)
-    print(file_path)
-    print(data)
--- 03_webapp/backend/server.py
+++ 03_webapp/backend/server.py
@@ -1,24 +1,19 @@
 from flask import Flask
-# from flask_cors import CORS  # need to mention
+from flask_cors import CORS  # need to mention
 import json
 import os
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 
 @app.route("/")
-def index():
-    return "Hello, index!"
-
-
-@app.route("/book")
-def book():
-    file_path = os.path.join(os.getcwd(), '03_webapp', 'backend', 'data.json')
+def json_file():
+    # file_path = os.path.join(os.getcwd(), 'backend', 'data.json')
+    file_path = './data.json'
     with open(file_path) as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5009)
+    app.run(host='0.0.0.0', port=9002)
